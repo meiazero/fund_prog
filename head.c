d18c69c2908868d16506d7a443b9044ff74bfa70
meiazero <avilaemanueel@gmail.com>
2022-06-22 23:31:18 -0300

    formatação

--- head.c
+++ head.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(){
 /* Modelo de cabecalho dos programas */
     printf("\t================= Bem Vindo ao Programa =================\n\n");
     /* fim do cabecalho */
+     //limpa a tela com delay de 6000ms
+    // system("clear");
+    // printf("Registrado!\n\n");
+    // usleep(600 * 1000);
+    // system("clear");
 
     return 0;
 }
